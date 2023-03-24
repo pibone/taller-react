35214ede3ccfb40f928d207f794ff05eab635350
Daniel Peña Iglesias <danpeis@gmail.com>
2023-03-24 18:51:56 +0100

    chore: vscode project settings and recommendations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Avoiding CORS issues 
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:slug*',
+  //       destination: 'http://localhost:3000/api/:slug*'
+  //     }
+  //   ]
+  // }
 }
 
 module.exports = nextConfig
